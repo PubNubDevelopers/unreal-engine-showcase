3b050b7253fd883b80b198d299cdab6923febdfa
oliverfcarson <oliver.carson@pubnub.com>
2024-09-10 14:31:45 -0700

    Front-End Chat UI Work, Bug Fixes,
    -Updating UI for displaying the missed messages notifications
    -Workaround for GetMemberships (currently not working as of the time of this check-in): utilize Unreal SDK to get memberships, parse JSON, and then handle.
    -Added workaround for also getting channel members. Need to update with chat sdk information, as it's storing the wrong channel id.

---
+++ Source/LyraGame/Messages/FChannelStruct.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+//#include "CoreMinimal.h" // Essential Unreal Engine includes
+#include "FChannelStruct.generated.h" // Required for USTRUCT macros
+
+USTRUCT(BlueprintType)
+struct FChannelStruct
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadWrite)
+	FString ChannelID;
+
+	UPROPERTY(BlueprintReadWrite)
+	FString Updated;
+
+	UPROPERTY(BlueprintReadWrite)
+	FString ETag;
+};
