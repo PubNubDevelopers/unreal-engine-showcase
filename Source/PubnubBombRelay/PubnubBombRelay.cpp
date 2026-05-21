d12e62619a5610936aaf316fba369e96b9480195
Kamil Gronek <kamil.gronek@pubnub.com>
2026-05-21 11:22:33 +0200

    Add new sample project.

---
+++ Source/PubnubBombRelay/PubnubBombRelay.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PubnubBombRelay.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PubnubBombRelay, "PubnubBombRelay" );
